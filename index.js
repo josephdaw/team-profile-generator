575df13218a17d83c83aa23b4338f80262cb0a94
josephdaw <josephdaw@icloud.com>
2021-11-10 00:17:42 +1030

    add links to required modules

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+
+// import custom modules
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
