1f0f961d578d6cf8e126d1d98a6353a122de36e4
josephdaw <josephdaw@icloud.com>
2021-11-11 02:30:56 +1030

    add getHTML method, add function to create index.html

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const Team = require('./lib/team');
-const fs = require('fs')
+const Html = require('./lib/html');
+const fs = require('fs');
 
 const team = new Team();
 
 const createTeam = async () => {
     await team.create();
-    let html = ""
+    let htmlTeam = ""
     team.members.forEach(member => {
-        html += member.getHTML();
+        htmlTeam += member.getHTML();
     });
-    console.log(html);
+    const htmlDoc = Html.htmlStart + htmlTeam + Html.htmlEnd;
+
+    console.log(htmlDoc);
+
     // function to create the HTML file
-    // fs.appendFile('index.html', data, (err) =>
-    //     err ? console.error('Error: ',err) : console.log('File created')
+    fs.appendFile('./dist/index.html', htmlDoc, (err) =>
+        err ? console.error('Error: ',err) : console.log('File created'))
 };
 
 createTeam();
